974cb214ad07e73de38dee06f36c4d1838a530a7
yyh-gl <yhonda.95.gl@gmail.com>
2018-11-12 13:29:11 +0900

    transactionを追加するフォーム追加

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,20 +1,26 @@
 class TransactionsController < ApplicationController
+
   def index
     if current_user
       @username = current_user.name
     else
       @username = 'hoge'
     end
-
-  end
-
-  def show
   end
 
   def create
+    new_transaction = Transaction.new(transaction_params)
+    new_transaction.save
   end
 
   # deviseのコントローラいじるのがめんどかったのでtransactionsコントローラにパスワード忘れたときようの処理を追加
   def forget
   end
+
+  private
+
+  def transaction_params
+    params.permit(:uid, :tyc, :reason)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+  get 'transactions/create'
   devise_for :users
 
   devise_scope :user do
     root 'devise/sessions#new'
   end
 
-  get '/transactions', to: 'transactions#index'
+  resources :transactions, only: [:index, :create]
+  
   # deviseのコントローラいじるのがめんどかったのでtransactionsコントローラにパスワード忘れたときようの処理を追加
   get '/users/password/forget', to: 'transactions#forget'
 end
