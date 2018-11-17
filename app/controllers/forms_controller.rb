829863de1ba702d751e612eedf86ed74ffecfdbf
yyh-gl <yhonda.95.gl@gmail.com>
2018-11-18 01:52:28 +0900

    WEB版見た目完成

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -11,6 +11,8 @@ class FormsController < ApplicationController
     @form.parameters.build
 
     @target_uids = User.all.map { |user| user[:uid] }
+
+    render template: 'forms/hoge'
   end
 
   def create
