d498d134c7e448ecec620205554f6bb40230daed
yyh-gl <yhonda.95.gl@gmail.com>
2018-11-21 18:31:41 +0900

    エラー修正

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require activestorage
+//= require turbolinks
 //= require jquery
 //= require jquery_ujs
 //= require nested_form_fields
-//= require activestorage
-//= require turbolinks
 //= require_tree .
